231792d1b547b67d14dd34cffa8da8d6735e7e73
Wendy Wu <wendy89711@gmail.com>
2018-12-11 11:57:38 +0800

    in class unfinished job

---
+++ src/com/exam/Order.java
@@ -0,0 +1,17 @@
+package com.exam;
+
+import java.util.ArrayList;
+
+public class Order {
+	ArrayList bag = new ArrayList();
+	public Order(){
+		for(int i = 0){
+			Ticket t = new Ticket(i);
+			
+		}
+		System.out.println();
+	}
+	public void print(){
+		
+	}
+}

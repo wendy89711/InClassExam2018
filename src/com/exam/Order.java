96bda0f7beb6196f5d819deceeef63870552471d
Wendy <wendy89711@gmail.com>
2019-01-07 12:51:02 +0800

    車票

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -3,15 +3,19 @@ package com.exam;
 import java.util.ArrayList;
 
 public class Order {
-	ArrayList bag = new ArrayList();
-	public Order() {
+	ArrayList ticket = new ArrayList();
+/*	public Order() {
 		Ticket ticket = new Ticket("Taipei", "Keelung", 37, 2);
 		bag.add(ticket);
-	}
-	public void add() {
-		
+	}*/
+	public void add(Ticket t) {
+		ticket.add(t);
 	}
 	public void print(){
-		
+		System.out.println("Result :");
+		for(int i = 0;i<ticket.size();i++) {
+			Ticket t = (Ticket)ticket.get(i);
+			System.out.println(t.get());
+		}
 	}
 }

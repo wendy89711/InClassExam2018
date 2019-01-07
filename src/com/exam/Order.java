a68a6c22b5032159821ca0d3d2e83949e4c2c25d
Wendy <wendy89711@gmail.com>
2019-01-07 14:38:32 +0800

    practice

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 
 public class Order {
 	ArrayList ticket = new ArrayList();
-/*	public Order() {
-		Ticket ticket = new Ticket("Taipei", "Keelung", 37, 2);
-		bag.add(ticket);
-	}*/
+	
 	public void add(Ticket t) {
 		ticket.add(t);
 	}
-	public void print(){
+	public void print() {
 		System.out.println("Result :");
 		for(int i = 0;i<ticket.size();i++) {
 			Ticket t = (Ticket)ticket.get(i);
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -6,14 +6,14 @@ public class Ticket {
 	int price;
 	int quantity;
 	
-	public Ticket (String origin,String destination,int price,int quantity){
+	public Ticket(String origin,String destination,int price,int quantity) {
 		this.origin = origin;
 		this.destination = destination;
 		this.price = price;
 		this.quantity = quantity;
 	}
 	public String get() {
-		String s = (origin)+" " +(destination)+ " " +(quantity)+" "+(price*quantity);
+		String s = (origin+" "+destination+" "+quantity+" "+(price*quantity));
 		return s;
 	}
 }
